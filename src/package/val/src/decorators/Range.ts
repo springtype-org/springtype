9d66b74733f38025c44185cafda5c02e1dfb7a64
Michael Mannseicher <michael.mannseicher@maibornwolff.de>
2018-09-27 13:57:36 +0200

    - added Range for iterate able objects

--- src/package/val/src/decorators/Range.ts
+++ src/package/val/src/decorators/Range.ts
@@ -3,7 +3,9 @@ import {validateRequired} from "./Required"
 import {validateDate as fromMaxValidateDate, validateNumber as fromMaxValidateNumber} from "./Max"
 import {validateDate as fromMinValidateDate, validateNumber as fromMinValidateNumber} from "./Min"
 import {validate as fromNumberValidate} from "./IsNumber"
+import {validate as froStringValidate} from "./IsString"
 import {validate as fromDateValidate} from "./IsDate";
+import {hasIterator} from "./NotEmpty";
 
 
 export const Range = <T extends number | Date>(minimum: T, maximum: T, options: Options = DECORATOR_OPTIONS_DEFAULT) =>
@@ -19,7 +21,12 @@ export const Range = <T extends number | Date>(minimum: T, maximum: T, options: 
                 (fromDateValidate(value)
                     && (fromDateValidate(minimum)
                         && fromDateValidate(maximum))
-                    && validateDate(value, <Date> minimum, <Date> maximum)),
+                    && validateDate(value, <Date> minimum, <Date> maximum))
+                ||
+                (hasIterator(value) && !froStringValidate(value)
+                    && (fromNumberValidate(minimum)
+                        && fromNumberValidate(maximum))
+                    && validateIterator(value, <number> minimum, <number> maximum)),
             options)
     );
 
@@ -28,3 +35,11 @@ export const validateNumber = (value: any, minimum: number, maximum: number): bo
 
 export const validateDate = (value: any, minimum: Date, maximum: Date): boolean =>
     fromMaxValidateDate(value, maximum) && fromMinValidateDate(value, minimum);
+
+export const validateIterator = (value: any, minimum: number, maximum: number): boolean => {
+    if (minimum < 0 || maximum < 0 || minimum > maximum) {
+        return false;
+    }
+    const length = value[Symbol.iterator].length;
+    return fromMaxValidateNumber(length, maximum) && fromMinValidateNumber(length, minimum);
+};
