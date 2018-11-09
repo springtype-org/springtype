e042fc73a4ec6eabd1d18df0b3d71f08f4195d98
mansi1 <m.mansi1@gmx.net>
2018-11-09 16:53:23 +0100

    added svg loader

---
+++ examples/svg-loader/src/svg-loader/SVGLoader.tsx
@@ -0,0 +1,27 @@
+import {WebComponent, WebComponentLifecycle} from "../../../../src/package/html";
+import template from "./SVGLoader.tpl";
+
+export interface LoaderProps {
+    type: LoaderType;
+    width: number;
+}
+
+export enum LoaderType {
+    STRANGE, CLOCK, RING, DOTS, DOT_BOUNCE, BOX_LAOD, CIRCLES, SOUND, SOUND_SMALL, CIRCLE
+}
+
+@WebComponent({
+    tag: 'svg-loader',
+    template
+})
+export class SVGLoader extends HTMLElement implements WebComponentLifecycle {
+
+    constructor(
+        public props: LoaderProps
+    ) {
+        super();
+        this.props.type = LoaderType.CIRCLE;
+        this.props.width = 100;
+    }
+
+}
