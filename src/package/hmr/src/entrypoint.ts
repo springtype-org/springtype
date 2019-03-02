368a49298587ce6d2f8540c63eb208c77af5fc18
Aron Homberg <info@aron-homberg.de>
2019-03-02 08:14:09 +0100

    Added documentation to the HMR module

--- src/package/hmr/src/entrypoint.ts
+++ src/package/hmr/src/entrypoint.ts
@@ -1,7 +1,13 @@
 export const hmrEntrypoint = (module: any) => {
+
     if (module) {
+
         (module as any).hot.dispose(() => {});
         (module as any).hot.accept(() => {
+
+            // make sure to hard-reload on hot module replacement
+            // to prevent odd/buggy behaviour with certain build systems
+            // (as cache invalidation isn't as easy as it seems to be, huh...)
             window.location.reload();
         });
     }
