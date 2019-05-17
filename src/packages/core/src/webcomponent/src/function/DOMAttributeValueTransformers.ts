fb97e2ed8e0c15e9dca936498761be9b69f11081
Michael Mannseicher <michael.mannseicher@maibornwolff.de>
2019-05-17 17:29:33 +0200

    added truthy check to onAttribute of type boolean

--- src/packages/core/src/webcomponent/src/function/DOMAttributeValueTransformers.ts
+++ src/packages/core/src/webcomponent/src/function/DOMAttributeValueTransformers.ts
@@ -1,13 +1,18 @@
-export const transformBooleanDOMAttributeValue = (value: string|boolean) => {
+export const transformBooleanDOMAttributeValue = (value: string | boolean) => {
     if (typeof value === 'boolean') {
         return value;
     } else {
-
+        if (value === 'false') {
+            return false;
+        }
+        if (value === 'true') {
+            return true;
+        }
         // i.e.: "disabled" on an HTML element ends in an empty string which should
         // be transformed to: true. Likewise disabled="disabled" should end up as true whereas
         // no presence at all should result in false.
         return typeof value !== 'undefined';
     }
 };
-export const transformFloatDOMAttributeValue = (value: string|number) => parseFloat(value.toString());
-export const transformIntDOMAttributeValue = (value: string|number) => parseInt(value.toString());
+export const transformFloatDOMAttributeValue = (value: string | number) => parseFloat(value.toString());
+export const transformIntDOMAttributeValue = (value: string | number) => parseInt(value.toString());
