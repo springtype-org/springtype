e4323c89523abf13a016cad517ab2650889958a8
Michael Mannseicher <m.mansi1@gmx.net>
2019-07-19 14:53:53 +0200

    removed default case in st-rm-rf

--- src/packages/cli/st-rm-rf/src/st-rm-rf.ts
+++ src/packages/cli/st-rm-rf/src/st-rm-rf.ts
@@ -12,33 +12,6 @@ const fsp = {
     access: promisify(fs.access),
 };
 
-
-const defaultRemovePathOrFile = async (deletePath: string, printError: boolean = false): Promise<boolean> => {
-    const stat = await fsp.lstat(deletePath);
-    if (stat.isDirectory()) {
-        const deletePaths = await fsp.readdir(deletePath);
-        for (let i = 0; i < deletePaths.length; i++) {
-            const _deletePath = deletePaths[i];
-            await removePathOrFile(path.join(deletePath, _deletePath));
-        }
-        await fsp.rmdir(deletePath);
-    } else if (stat.isFile() || stat.isSymbolicLink()) {
-        try {
-            await fsp.unlink(deletePath)
-            //file removed
-        } catch (err) {
-            if (printError) {
-                console.log(`- error ${err}`);
-            }
-            return false;
-        }
-    } else {
-        console.log(`- error ${deletePath}`);
-        return false;
-    }
-    return true;
-};
-
 export const filePathExist = async (existFilePath: string, printError: boolean = false): Promise<boolean> => {
     try {
         await fsp.access(existFilePath);
@@ -51,26 +24,20 @@ export const filePathExist = async (existFilePath: string, printError: boolean =
     return false;
 };
 
-
 export const removePathOrFile = async (deletePath: string, printError: boolean = false): Promise<boolean> => {
-    const platform = os.platform();
     try {
-        switch (platform) {
-            case "win32":
-                const stat = await fsp.lstat(deletePath);
-                if (stat.isDirectory()) {
-                    process.execSync(`rmdir /s /q "${deletePath}"`);
-                } else {
-                    process.execSync(`del "${deletePath}"`);
-                }
-                return true;
-            case "linux":
-                process.execSync(`rm -rf "${deletePath}"`);
-                return true;
-            default:
-                //unknown platform
-                console.log(`Unsupported platform (${platform}) run default`);
-                return await defaultRemovePathOrFile(deletePath, printError);
+        const platform = os.platform();
+        if (platform === "win32") {
+            const stat = await fsp.lstat(deletePath);
+            if (stat.isDirectory()) {
+                process.execSync(`rmdir /s /q "${deletePath}"`);
+            } else {
+                process.execSync(`del "${deletePath}"`);
+            }
+            return true;
+        } else {
+            process.execSync(`rm -rf "${deletePath}"`);
+            return true;
         }
     } catch (err) {
         if (printError) {
