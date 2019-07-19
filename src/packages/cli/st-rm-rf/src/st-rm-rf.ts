7860cc62d8ee79f742c090d55e512a95bc9f732e
mansi1 <m.mansi1@gmx.net>
2019-07-19 09:17:35 +0200

    - updated st-rm-rf

--- src/packages/cli/st-rm-rf/src/st-rm-rf.ts
+++ src/packages/cli/st-rm-rf/src/st-rm-rf.ts
@@ -1,8 +1,21 @@
+import {ExecException} from "child_process";
+
 const path = require('path');
 const fs = require('fs');
-const fsp = fs.promises;
+const promisify = require('util').promisify;
+const process = require("child_process");
+const os = require("os");
 
-export const removePathOrFile = async (deletePath: string, printError: boolean = false): Promise<boolean> => {
+const fsp = {
+    lstat: promisify(fs.lstat),
+    readdir: promisify(fs.readdir),
+    rmdir: promisify(fs.rmdir),
+    unlink: promisify(fs.unlink),
+    access: promisify(fs.access),
+};
+
+
+const defaultRemovePathOrFile = async (deletePath: string, printError: boolean = false): Promise<boolean> => {
     const stat = await fsp.lstat(deletePath);
     if (stat.isDirectory()) {
         const deletePaths = await fsp.readdir(deletePath);
@@ -38,4 +51,33 @@ export const filePathExist = async (existFilePath: string, printError: boolean =
         }
     }
     return false;
+};
+
+
+export const removePathOrFile = async (deletePath: string, printError: boolean = false): Promise<boolean> => {
+    const platform = os.platform();
+    try {
+        switch (platform) {
+            case "win32":
+                const stat = await fsp.lstat(deletePath);
+                if (stat.isDirectory()) {
+                    process.execSync(`rmdir /s /q "${deletePath}"`);
+                } else {
+                    process.execSync(`del "${deletePath}"`);
+                }
+                return true;
+            case "linux":
+                process.execSync(`rm -rf "${deletePath}"`);
+                return true;
+            default:
+                //unknown platform
+                console.log(`Unsupported platform (${platform}) run default`);
+                return await defaultRemovePathOrFile(deletePath, printError);
+        }
+    } catch (err) {
+        if (printError) {
+            console.log(`- error ${err}`);
+        }
+        return false;
+    }
 };
