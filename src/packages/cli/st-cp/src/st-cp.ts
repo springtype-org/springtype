7574df4e69b5075ebda6f7dbddcf164517b93f67
mansi1 <m.mansi1@gmx.net>
2019-08-28 15:57:01 +0200

    fixes for windows compatibility

--- src/packages/cli/st-cp/src/st-cp.ts
+++ src/packages/cli/st-cp/src/st-cp.ts
@@ -1,11 +1,23 @@
 const process = require("child_process");
 const os = require("os");
+const fs = require('fs');
+const promisify = require('util').promisify;
+
+const fsp = {
+    lstat: promisify(fs.lstat)
+};
 
 export const copyPathOrFile = async (sourcePath: string, destinationPath: string, printError: boolean = false): Promise<boolean> => {
     try {
         const platform = os.platform();
         if (platform === "win32") {
-            process.execSync(`xcopy "${sourcePath}" "${destinationPath}" /O /X /E /H /K`);
+            const stat = await fsp.lstat(sourcePath);
+            if (stat.isDirectory()) {
+                process.execSync(`xcopy "${sourcePath}" "${destinationPath}" /O /X /E /H /K`);
+            } else {
+                process.execSync(`xcopy "${sourcePath}" "${destinationPath}"`);
+            }
+
             return true;
         } else {
             process.execSync(`cp -rp "${sourcePath}" "${destinationPath}"`);
