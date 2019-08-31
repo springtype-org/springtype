3fce98b1a8ebe2bb8843abbb970e5ab3be5300a9
kyr0 <info@aron-homberg.de>
2019-08-31 03:35:49 +0200

    feat(material-ui): Added index page

--- src/packages/material-ui/doc/TestApp.tsx
+++ src/packages/material-ui/doc/TestApp.tsx
@@ -14,11 +14,13 @@ import {TextfieldPage} from "./page/textfield-page/textfield-page";
 import {CheckboxPage} from "./page/checkbox-page/checkbox-page";
 import {SwitchPage} from "./page/switch-page/switch-page";
 import {TobBarPage} from "./page/tob-bar-page/tob-bar-page";
+import {IndexPage} from "./page/index-page/index-page";
 
 @Route('mwc-button', ButtonPage)
 @Route('mwc-textfield', TextfieldPage)
 @Route('mwc-checkbox', CheckboxPage)
 @Route('mwc-switch', SwitchPage)
 @Route('mwc-top-bar', TobBarPage)
+@Route('*', IndexPage)
 export class TestApp {
 }
