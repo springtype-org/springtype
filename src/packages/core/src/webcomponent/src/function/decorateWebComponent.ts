70808c06e634c1f9addfe256effc858a91b64e97
kyr0 <info@aron-homberg.de>
2019-09-03 01:48:04 +0200

    fix: HMR reloadability: define without safety measures

--- src/packages/core/src/webcomponent/src/function/decorateWebComponent.ts
+++ src/packages/core/src/webcomponent/src/function/decorateWebComponent.ts
@@ -7,26 +7,20 @@ import {installInitialMutationObserver} from "./installInitialMutationObserver";
 export const decorateWebComponent = (tagName: string, webComponent: ComponentImpl<any>) => {
 
     let CustomWebComponent;
-    const registeredCustomWebComponent = window.customElements.get(tagName);
 
-    if (!registeredCustomWebComponent) {
+    // @Component by default
+    const injectableWebComponent = Component(webComponent);
+    CustomWebComponent = createWebComponentClass(tagName, injectableWebComponent);
+    // register custom element
+    window.customElements.define(tagName, CustomWebComponent);
 
-        // @Component by default
-        const injectableWebComponent = Component(webComponent);
-        CustomWebComponent = createWebComponentClass(tagName, injectableWebComponent);
-        // register custom element
-        window.customElements.define(tagName, CustomWebComponent);
+    WebComponentReflector.setTagName(<any>CustomWebComponent, tagName);
 
-        WebComponentReflector.setTagName(<any>CustomWebComponent, tagName);
+    WebComponentReflector.registerByTagName(tagName, CustomWebComponent);
 
-        WebComponentReflector.registerByTagName(tagName, CustomWebComponent);
+    ComponentReflector.addInitializer(CustomWebComponent, (instance: any) => {
+        installInitialMutationObserver(instance, tagName);
+    });
 
-        ComponentReflector.addInitializer(CustomWebComponent, (instance: any) => {
-            installInitialMutationObserver(instance, tagName);
-        });
-
-    } else {
-        CustomWebComponent = WebComponentReflector.getByTagName(tagName);
-    }
     return CustomWebComponent;
 };
