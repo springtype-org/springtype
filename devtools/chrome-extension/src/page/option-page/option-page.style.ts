c68054cbef85d104748d155592697a8d83476286
kyr0 <info@aron-homberg.de>
2019-10-26 20:01:01 +0200

    chore: Removed devtools because of migration to other repo

--- devtools/chrome-extension/src/page/option-page/option-page.style.ts
+++
@@ -1,8 +0,0 @@
-import {OptionPage} from "./option-page";
-import {ITypedStyleSheet} from "../../../../../src/web/tss";
-
-export default (component: OptionPage): ITypedStyleSheet => ({
-    "option-page": {
-
-    }
-});
--- devtools/chrome-extension/src/page/option-page/option-page.tpl.tsx
+++
@@ -1,8 +0,0 @@
-import {OptionPage} from "./option-page";
-import {tsx} from "../../../../../src/web/vdom";
-import {IVirtualNode} from "../../../../../src/web/vdom/interface/IVirtualNode";
-
-export default (component: OptionPage):IVirtualNode =>
-    <p>Component: OptionPage</p>
-;
-
