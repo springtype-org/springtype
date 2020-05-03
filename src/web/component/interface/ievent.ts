08e73b54e09406de992070d82db2b24914ff1679
kyr0 <info@aron-homberg.de>
2020-05-03 11:03:31 +0200

    Fixed IEvent typing

--- src/web/component/interface/ievent.ts
+++ src/web/component/interface/ievent.ts
@@ -1,5 +1,8 @@
 export interface IEvent<D> {
-  detail: D;
+
+  // it is optional, because one can also disptach native DOM events as a relay
+  // those events don't have any detail member property
+  detail?: D;
 }
 
 export type IEventListener<D> = (evt: IEvent<D> & Event) => void | boolean;
